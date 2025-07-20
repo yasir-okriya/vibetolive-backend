a466d261570dd31bb47da40b1811afab844c72b1
Okriya <okriya@Okriyas-MacBook-Pro.local>
2025-07-20 17:42:42 +0600

    static file route fixed

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+const uploadsPath = path.join(__dirname, 'app/uploads');
+app.use('/uploads', express.static(uploadsPath));
 
 
 app.use('/api', router);
